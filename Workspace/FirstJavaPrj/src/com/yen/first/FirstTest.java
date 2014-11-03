29bdbd0223bd03772b5e63e48f04bdf1254833db
YaronEngel <yaron.engel@web.de>
2014-11-03 10:29:29 +0100

    FirstCommit

---
+++ Workspace/FirstJavaPrj/src/com/yen/first/FirstTest.java
@@ -0,0 +1,30 @@
+/**
+ * Engel2
+ * 03.11.2014
+ */
+package com.yen.first;
+
+
+/**
+ * FirstTest TODO
+ * @author Engel2
+ */
+public class FirstTest {
+
+    /**
+     * Constructor.
+     */
+    public FirstTest() {}
+
+    /**
+     * TODO
+     * @author Engel2
+     * @since 03.11.2014
+     * @param args
+     * <code>void</code> - 
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
